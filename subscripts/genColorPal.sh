c814c03b6d8862aec2f684b6521d98c87eab5554
ardisprime <christoffer.marschner@online.de>
2025-03-03 14:08:48 +0100

    move main script into bin

--- subscripts/genColorPal.sh
+++ subscripts/genColorPal.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+backgrounds="$HOME/backgrounds"
+
 # gen colors
-magick $1 -gamma 0.2 -colors 2 -unique-colors txt:pallette.txt
+magick $1 -gamma 0.2 -colors 2 -unique-colors txt:$backgrounds/pallette.txt
 
 # clean up colorfile
-sed -e '2,3s/^.*#//' -e '2,3s/ .*$//' -e '2,3s/..$//' -e '1d' -i pallette.txt
+sed -e '2,3s/^.*#//' -e '2,3s/ .*$//' -e '2,3s/..$//' -e '1d' -i $backgrounds/pallette.txt
 
--- subscripts/setColors.sh
+++ subscripts/setColors.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+backgrounds="$HOME/backgrounds"
+
 # get colors from pallette
-colorbg=$(sed -n "1p" pallette.txt)
-colorfg=$(sed -n "2p" pallette.txt)
+colorbg=$(sed -n "1p" $backgrounds/pallette.txt)
+colorfg=$(sed -n "2p" $backgrounds/pallette.txt)
 
 # ghostty
 ghostty="$HOME/.config/ghostty"
